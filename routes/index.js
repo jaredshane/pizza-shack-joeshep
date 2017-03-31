aba0374fc797a5a8c045de2b6568eb4d00851e9c
Jared Fuller <jaredshane@gmail.com>
2017-03-31 09:34:46 -0500

    added router for index

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const { Router } = require('express')
+const router = Router()
+
+
+//public routes
+router.use(require('./about'))
+router.use(require('./contact'))
+router.use(require('./login'))
+// router.use(require('./register'))
+router.use(require('./root'))
+
+//login guard middleware
+// router.use( (req, res, next) => {
+//   if (req.isAuthenticated()) {
+//     next()
+//   } else {
+//     res.redirect('/login')
+//   }
+// })
+
+//private routes
+
+// router.use(require('./logout'))
+// router.use(require('./orders'))
+
+
+module.exports = router
