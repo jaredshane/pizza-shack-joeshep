7203a3aceb39625f3fa5ee5c7b317e582379c102
Jared Fuller <jaredshane@gmail.com>
2017-03-31 09:34:36 -0500

    added router for the contact page

---
+++ routes/contact.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const { Router } = require('express')
+
+const { show } = require('../controllers/contactCtrl')
+
+const router = Router()
+
+router.get('/contact', show)
+module.exports = router
