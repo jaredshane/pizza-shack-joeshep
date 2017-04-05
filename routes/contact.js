bc9929697d632798baf0caf6eb5f413feee38080
Jared Fuller <jaredshane@gmail.com>
2017-04-05 15:00:57 -0500

    required in addContact and set the post action to post the info of database

--- routes/contact.js
+++ routes/contact.js
@@ -2,9 +2,12 @@
 
 const { Router } = require('express')
 
-const { show } = require('../controllers/contactCtrl')
+const { show, addContact } = require('../controllers/contactCtrl')
 
 const router = Router()
 
 router.get('/contact', show)
+
+router.post('/contact', addContact)
+
 module.exports = router
