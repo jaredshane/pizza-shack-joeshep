e75a69ad9df431132cfef3b8cbf39dd8b886d8e6
Jared Fuller <jaredshane@gmail.com>
2017-04-05 15:08:12 -0500

    set the route to handle when getting a get request from /register

---
+++ routes/register.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const { Router } = require('express')
+
+const { show } = require('../controllers/registerCtrl')
+
+const router = Router()
+
+router.get('/register', show)
+module.exports = router
