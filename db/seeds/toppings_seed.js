7939f48134cfca4573bb7bb6128c234567e0feff
Jared Fuller <jaredshane@gmail.com>
2017-04-07 15:08:36 -0500

    changed table name in the seed after changing the name of the table in the new database

--- db/seeds/toppings_seed.js
+++ db/seeds/toppings_seed.js
@@ -3,10 +3,10 @@ const toppings = require('./toppings');
 
 exports.seed = (knex, Promise) => {
   let toppingsPromises = toppings.map( ({name}) => {
-    return knex('topping').insert({name});
+    return knex('toppings').insert({name});
   });
   // Deletes ALL existing entries
-  return knex('topping').del()
+  return knex('toppings').del()
     .then( () => {
       // Inserts seed entries
       return Promise.all(toppingsPromises);
