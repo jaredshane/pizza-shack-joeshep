2ee1134c5b7e5d1549ec55d35a04c06e65e35598
Jared Fuller <jaredshane@gmail.com>
2017-04-07 14:59:22 -0500

    required in new dependencies

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const flash = require('express-flash')
+const session = require('express-session')
+const passport = require('passport')
+const KnexSessionStore = require('connect-session-knex')(session)
+const { knex } = require('./db/database')
 
 const routes = require('./routes')
 
