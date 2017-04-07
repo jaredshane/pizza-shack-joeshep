b5d4621873bd5203abb34e09e13b6bf473c641cf
Jared Fuller <jaredshane@gmail.com>
2017-04-07 15:06:15 -0500

    uncommented out a require for bcyrpt

--- db/database.js
+++ db/database.js
@@ -5,7 +5,8 @@ const config = require('../knexfile')[environment]
 const knex = require('knex')(config)
 const bookshelf = require('bookshelf')(knex);
 
-// bookshelf.plugin(require('bookshelf-bcrypt'))
+//Allows us to hash the users passwords before saving them
+bookshelf.plugin(require('bookshelf-bcrypt'))
 
 
 module.exports = { knex, bookshelf }
