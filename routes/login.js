ee96e56392d4f20de822302f9f8536d25eed1302
Jared Fuller <jaredshane@gmail.com>
2017-04-07 15:20:04 -0500

    required in create from sessionCtrl and handled a post action

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,12 @@
-'use strict';
+'use strict'
 
 const { Router } = require('express')
 
-const { show } = require('../controllers/loginCtrl')
+const { show, create } = require('../controllers/sessionCtrl')
 
 const router = Router()
 
 router.get('/login', show)
+router.post('/login', create)
+
 module.exports = router
